55b203d589d6b8921581ff2055fe3839f3231585
Francsy <francsyludens@gmail.com>
2023-03-04 14:05:17 +0100

    add/connection to data base

--- index.js
+++ index.js
@@ -2,10 +2,8 @@ const express = require('express')
 const morgan = require('morgan')
 const errorHandler = require('./middlewares/errorHandler')
 
-
 const app = express()
 const port = 3000
-
 
 app.use(morgan('dev'))
 
