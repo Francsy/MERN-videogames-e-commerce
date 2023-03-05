799feff28cba64e1ef207b81e8df983e552c3b45
Francsy <francsyludens@gmail.com>
2023-03-05 11:24:52 +0100

    add/script for froont and back

--- index.js
+++ index.js
@@ -1,27 +1,23 @@
 const express = require('express')
 const morgan = require('morgan')
 const errorHandler = require('./middlewares/errorHandler')
-
-
+require('dotenv').config()
 
 
 const productsApiRoutes = require('./routes/productsApiRoutes')
 
-
-
 const app = express()
-const port = 3000
+const PORT = process.env.PORT || 5000;
 
 app.use(morgan('dev'))
 
 app.use('/api/products',productsApiRoutes); // Rutas web API products
 
 
-
-app.get('/', (req, res) => res.send('Hello World'))
+app.get('/', (req, res) => res.send('Welcome to our api'))
 
 app.use(errorHandler);
 
-app.listen(port, () => {
-    console.log('Working on port ' + port)
+app.listen(PORT, () => {
+    console.log('Working on port ' + PORT)
 })
