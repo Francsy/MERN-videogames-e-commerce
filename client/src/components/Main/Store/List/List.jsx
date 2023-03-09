d654a84af46eb5d3afe4f6e0e7442762f2a48377
Francsy <francsyludens@gmail.com>
2023-03-09 12:32:51 +0100

    add/no products condition solution + .env.example

--- client/src/components/Main/Store/List/List.jsx
+++ client/src/components/Main/Store/List/List.jsx
@@ -9,7 +9,7 @@ const List = (props) => {
 
 <div className="bg-white">
       <div className="mx-auto max-w-2xl py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
-        <h2 className="text-2xl font-bold tracking-tight text-gray-900">Results:</h2>
+        <h2 className="text-2xl font-bold tracking-tight text-gray-900">{props.products.length > 0 ?"Results:" : "No products or distributors matched your search."}</h2>
         <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
     {printProducts()}          
         </div>
