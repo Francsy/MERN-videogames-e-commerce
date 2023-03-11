eaaf21b37f6f392c9793e70213f80015e7e36515
Francsy <francsyludens@gmail.com>
2023-03-11 20:53:27 +0100

    add / server.js changes from last commit

--- server.js
+++ server.js
@@ -15,10 +15,17 @@ const PORT = process.env.PORT || 5000;
 app.use(cors())
 app.use(morgan('dev'))
 
-app.use('/api/products',productsApiRoutes); // Rutas web API products
-
+app.use('/api/products', productsApiRoutes); // Rutas web API products
 
 app.get('/', (req, res) => res.send('Welcome to our api'))
+
+if (process.env.NODE_ENV === 'production') {
+    //*Set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
+}
+
 
 app.use(errorHandler);
 
