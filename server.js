c625c0a1c05cd2919c49080ea78e168c4017d2c1
Francsy <francsyludens@gmail.com>
2023-03-12 15:15:51 +0100

    add /express.json

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const path = require('path');
 require('dotenv').config()
 
 
-const morgan = require('morgan')
 const errorHandler = require('./middlewares/errorHandler')
 
 
@@ -14,7 +13,8 @@ const app = express()
 const PORT = process.env.PORT || 5000;
 
 app.use(cors())
-app.use(morgan('dev'))
+app.use(express.json({ extended: false }));
+
 
 app.use('/api/products', productsApiRoutes); // Rutas web API products
 
