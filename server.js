c4f81cf794d401b9734896f80eff96452c7a55b3
Francsy <francsyludens@gmail.com>
2023-03-12 19:54:10 +0100

    add/docker configuration files and path

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const cors = require('cors')
+const path = require('path');
 require('dotenv').config()
 
 
@@ -17,15 +18,12 @@ app.use(morgan('dev'))
 
 app.use('/api/products', productsApiRoutes); // Rutas web API products
 
-app.get('/', (req, res) => res.send('Welcome to our api'))
-
 if (process.env.NODE_ENV === 'production') {
     //*Set static folder
     app.use(express.static('client/build'));
-
+    
     app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
 }
-
 
 app.use(errorHandler);
 
